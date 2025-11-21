377a9f194a7753eac3cd44b47dddf07da58762ae
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-11-21 18:07:52 +1030

    created class animal

--- animal.py
+++ animal.py
@@ -1,8 +1,17 @@
-'''
+"""
 File: filename.py
 Description: A brief description of this Python module.
 Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, name, animal_id, age, species, dietary_needs):
+        self.name = name
+        self.animal_id = animal_id
+        self.age = age
+        self.species = species
+        self.dietary_needs = dietary_needs
