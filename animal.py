6df0d648ddd0ad22a3e6e4770af2a85534f622bf
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-11-24 01:32:57 +1030

    Added getters, setters, sound logic, and str method to Mammal.

--- animal.py
+++ animal.py
@@ -58,13 +58,25 @@ class Mammal(Animal):
         super().__init__(name, animal_id, age, species, dietary_needs)
         self.__fur_colour = fur_colour
 
-    def get_fur_color(self):
+    def get_fur_colour(self):
         return self.__fur_colour
 
     def set_fur_colour(self, fur_colour):
         self.__fur_colour = fur_colour
 
+    def make_sound(self):
+        if "lion" in self.get_species().lower():
+            return f"{self.get_name()} does roars loudly."
+        elif "elephant" in self.get_species().lower():
+            return f"{self.get_name()} trumpets loudly with its trunk."
+        else:
+            return f"{self.get_name()} does make a mammal sound."
 
+    def eat(self):
+        return f"{self.get_name()} eats {self.get_dietary_needs()}."
+
+    def __str__(self):
+        return super().__str__() + f", Fur colour: {self.__fur_colour}"
 
 
 
