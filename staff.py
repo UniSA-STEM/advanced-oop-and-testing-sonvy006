f3aa2b4dcd56c15d66017f71a11246c4505954a8
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-11-24 18:07:41 +1030

    Create abstract Staff class and implement staff class structure.

--- staff.py
+++ staff.py
@@ -1,8 +1,24 @@
-'''
-File: filename.py
+"""
+File: staff.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Vishesh Soni
+ID: 110387138
+Username: sonvy006
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+
+from abc import ABC, abstractmethod
+
+class Staff(ABC):
+
+    def __init__(self, staff_id, name, role):
+        self.__staff_id = staff_id
+        self.__name = name
+        self.__role = role
+        self.__assigned_animals = []
+        self.__assigned_enclosures = []
+
+
+
+
+
