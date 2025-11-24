52fa92ac17034b1f24d437e4f5daaaec043e0798
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-11-24 19:00:22 +1030

    Add getters and logic to assign animals and enclosures

--- staff.py
+++ staff.py
@@ -18,6 +18,41 @@ class Staff(ABC):
         self.__assigned_animals = []
         self.__assigned_enclosures = []
 
+    def get_staff_id(self):
+        return self.__staff_id
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_role(self):
+        return self.__role
+
+    def get_assigned_animals(self):
+        return self.__assigned_animals
+
+    def assign_animal(self, animal):
+        if animal not in self.__assigned_animals:
+            self.__assigned_animals.append(animal)
+
+    def unassign_animal(self, animal):
+        if animal in self.__assigned_animals:
+            self.__assigned_animals.remove(animal)
+
+    def get_assigned_enclosures(self):
+        return self.__assigned_enclosures
+
+    def assign_enclosure(self, enclosure):
+        if enclosure not in self.__assigned_enclosures:
+            self.__assigned_enclosures.append(enclosure)
+
+    def unassign_enclosure(self, enclosure):
+        if enclosure in self.__assigned_enclosures:
+            self.__assigned_enclosures.remove(enclosure)
+
+
 
 
 
